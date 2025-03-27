328f21a12574e281375d27093e0376ef5b89a9ed
scy2000-lab <513ab@naver.com>
2025-03-27 17:11:38 +0900

    refactoring

---
+++ skala/src/main/java/com/sk/skala/SkalaApplication.java
@@ -0,0 +1,13 @@
+package com.sk.skala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkalaApplication.class, args);
+	}
+
+}
---
+++ skala/src/test/java/com/sk/skala/SkalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.skala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
